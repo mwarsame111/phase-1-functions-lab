8332286f79a5f6bcd66cb2e526ce9112da585f2c
mohamed warsame <mwarsame111@gmail.com>
2023-06-20 16:09:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if (someValue > 42) {
+        distanceFromHqInBlocks = someValue - 42
+        return distanceFromHqInBlocks
+    } else {
+        distanceFromHqInBlocks = 42 - someValue
+        return distanceFromHqInBlocks
+    }
+}
+
+function distanceFromHqInFeet(someValue){
+    distanceFromHqInFeet = 264;
+    someValue * distanceFromHqInBlocks
+    return distanceFromHqInFeet
+}
